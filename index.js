f38188f735e685dd2822c4dcc1f210734b0576dc
R-I-F <ibrahimrefaeei@gmail.com>
2023-07-27 18:04:45 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const inputEl = document.getElementById("input-el")
+const btnEl = document.getElementById("btn-el")
+const lengthTxt = document.getElementById("length-txt")
+const volumeTxt = document.getElementById("volume-txt")
+const massTxt = document.getElementById("mass-txt")
+const x = document.getElementById("error")
+
+
+
+
+function render (){
+    let metersFeet =inputEl.value*3.281
+    let feetMeters =inputEl.value/3.281
+    let litersGallons = inputEl.value*0.264
+    let gallonsLiters = inputEl.value/0.264
+    let kilogramPound = inputEl.value*2.204
+    let poundKilogram = inputEl.value/2.204
+    lengthTxt.innerHTML=`<p class="inner-txt">${inputEl.value} meters = ${metersFeet.toFixed(2)} feet | ${inputEl.value} Feet = ${feetMeters.toFixed(2)} Meters</p>`
+    volumeTxt.innerHTML=`<p class="inner-txt">${inputEl.value} liters = ${litersGallons.toFixed(2)} Gallons | ${inputEl.value} Gallons = ${gallonsLiters.toFixed(2)} liters</p>`
+    massTxt.innerHTML=`<p class="inner-txt">${inputEl.value} Kilogram = ${kilogramPound.toFixed(2)} Pound | ${inputEl.value} Pound = ${poundKilogram.toFixed(2)} Kilogram</p>`
+}   
+
+btnEl.addEventListener("click", function(){
+    if (inputEl.value){
+        render ()
+        inputEl.value = ""
+        x.innerHTML = ""
+    }
+    else{
+        x.innerHTML = `<h2 class="inner-txt">Please insert a value to convert!</h2>`
+    }
+})
